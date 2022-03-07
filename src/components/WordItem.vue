99a89e961f2521373d93a1d66ca23436d77cf830
t_mori <t_mori@cosmoroot.co.jp>
2022-03-07 19:06:52 +0900

    impl review view

---
+++ src/components/WordItem.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="box word-review has-pointer">
+		<span class="icon is-medium has-pointer material-icons-outlined is-circled has-background-white-ter">keyboard_voice</span>	
+		<div class="word-review-contents">
+			<p class="body">{{word}}</p>
+			<P>{{translate}}</P>
+		</div>
+		<p class="body word-review-eval">{{text}}</p>
+		<input type="checkbox" class="input word-review-checkbox" :checked="checked">
+	</div>
+</template>
+
+<script lang="ts" setup>
+import { toRefs,ref} from 'vue'
+
+type Props= {
+	word:string,
+	translate:string,
+	mistake:boolean,
+}
+
+const props = defineProps<Props>()
+const {word,translate,mistake} = toRefs(props)
+
+const checked = ref<boolean>(mistake.value)
+const text = mistake.value ? 'BAD':'GOOD'
+
+
+</script>
+
+
+<style lang="sass" scoped>
+
+.word-review
+	margin: 0 auto
+	display: flex
+	flex-direction: row
+	justify-content: space-between
+	align-items: center
+	+ .word-review
+			margin-top: 12px
+
+	&-contents
+		margin-right: auto
+		margin-left: 30px
+		> p
+				margin: .5em 0
+	&-eval
+		margin-right: .5em
+	&-checkbox
+		width: 16px
+		height: 16px
+		vertical-align: baseline
+
+</style>
--- src/views/Review.vue
+++ src/views/Review.vue
@@ -1,5 +1,22 @@
 <template>
-	<div>
-		Review
-	</div>
+	<div class="container">
+		<section class="preview-section">
+			<p class="body">単語一覧</p>
+			<WordItem word="transfer" translate="移す" :mistake="false"/>
+			<WordItem word="transfer" translate="移す" :mistake="false"/>
+		</section>
+</div>
 </template>
+
+<script lang="ts" setup>
+import WordItem from "@/components/WordItem.vue"
+
+
+</script>
+
+<style lang="sass" scoped>
+.preview-section
+	width: 60%
+	max-width: 600px
+	margin: 0 auto
+</style>
